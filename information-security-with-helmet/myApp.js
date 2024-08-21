efda7d3f49eeb60ea4bb7d330bc00f7e4d4c56b0
alexis-massa <alexis.massa351@gmail.com>
2024-08-21 08:33:29 +0200

    Information Security with HelmetJS : Done

---
+++ information-security-with-helmet/myApp.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express()
+module.exports = app;
+const api = require('./server.js');
+
+const helmet = require('helmet');
+app.use(helmet({
+  frameguard: { action: 'deny' },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "trusted-cdn.com"],
+    }
+  },
+  dnsPrefetchControl: false
+}))
+
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
